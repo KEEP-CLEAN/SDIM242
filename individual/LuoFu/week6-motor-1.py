231c36a1b43ced468708d96355640b06989498b4
L-fu-des22 <71368999+L-fu-des22@users.noreply.github.com>
2020-10-20 00:04:28 +0800

    Add files via upload

---
+++ individual/LuoFu/week6-motor-1.py
@@ -0,0 +1,63 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setwarnings(False)
+
+out1 = 35
+out2 = 36
+out3 = 37
+out4 = 38
+
+ENA = 31
+ENB = 32
+
+i=0
+positive=0
+negative=0
+y=0
+
+
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(out1,GPIO.OUT)
+GPIO.setup(out2,GPIO.OUT)
+GPIO.setup(out3,GPIO.OUT)
+GPIO.setup(out4,GPIO.OUT)
+GPIO.setup(ENA,GPIO.OUT)
+GPIO.setup(ENB,GPIO.OUT)
+print( "First calibrate by giving some +ve and -ve values.....")
+
+pwm1 = GPIO.PWM(ENA, 50)
+pwm2 = GPIO.PWM(ENB, 50)
+pwm1.start(0)
+pwm2.start(0)
+
+while(1):
+    GPIO.output(out1,GPIO.LOW)
+    GPIO.output(out2,GPIO.LOW)
+    GPIO.output(out3,GPIO.LOW)
+    GPIO.output(out4,GPIO.LOW)
+    x1=int(input())
+    x2=int(input())
+    pwm1.ChangeDutyCycle(x1)
+    pwm2.ChangeDutyCycle(x1)
+    if x2==0:
+        GPIO.output(out1,GPIO.LOW)
+        GPIO.output(out2,GPIO.HIGH)
+        GPIO.output(out3,GPIO.LOW)
+        GPIO.output(out4,GPIO.HIGH)
+        time.sleep(3)
+    else:
+        GPIO.output(out1,GPIO.HIGH)
+        GPIO.output(out2,GPIO.LOW)
+        GPIO.output(out3,GPIO.HIGH)
+        GPIO.output(out4,GPIO.LOW)
+        time.sleep(3)
+
+GPIO.output(out1,GPIO.LOW)
+GPIO.output(out2,GPIO.LOW)
+GPIO.output(out3,GPIO.LOW)
+GPIO.output(out4,GPIO.LOW)
+GPIO.cleanup()
+pwm1.stop()
+pwm2.stop()
+    
